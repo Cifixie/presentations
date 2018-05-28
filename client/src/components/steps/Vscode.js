3f4b5385c79beb7997e40f2628e759cc0e553c9b
Tommi <mato12@hotmail.fi>
2018-05-28 16:32:10 +0300

    vscode

--- client/src/components/steps/Vscode.js
+++ client/src/components/steps/Vscode.js
@@ -1,11 +1,22 @@
 import React, { Fragment } from "react";
+import styled from "styled-components";
 import { List } from "../Components";
+
+const A = styled.a`
+  margin-left: 0.5em;
+`;
 
 export default () => (
   <Fragment>
     <h1>Vscode integrations</h1>
-
+    <p>Debug in same environment where you code</p>
     <List>
+      <li>
+        Add
+        <A href="https://github.com/Microsoft/vscode-chrome-debug">
+          an extenssion
+        </A>
+      </li>
       <li />
     </List>
   </Fragment>
